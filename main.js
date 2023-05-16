b4ff48340c7908d17c4b67fec32b6cd2e3d2b262
Pawel Ochmann <pawelochmann@poczta.onet.pl>
2023-05-16 18:18:52 +0200

    Create second module, connect it to previous one by function it returns, now human players are able to click and leave their signs

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-(function mainModule() {
-  player1 = {};
-  player2 = {};
+const mainModule = (function () {
+  let player1 = {};
+  let player2 = {};
   function playerCreate(sign, name) {
-    return { sign, name };
+    let turn = true;
+    if (sign === 'o') {
+      turn = false;
+    }
+    return { sign, name, turn };
   }
+
   function addPlayerButtonHandler() {
     const sign = this.dataset.sign;
     const box = document.querySelector(`div[data-sign='${sign}']`);
@@ -19,4 +24,46 @@
   addPlayerButtons.forEach((button) => {
     button.addEventListener('click', addPlayerButtonHandler);
   });
+
+  function toggleTurn() {
+    player1.turn = !player1.turn;
+    player2.turn = !player2.turn;
+    const fieldset = document.querySelectorAll('fieldset');
+    fieldset.forEach((f) => {
+      f.classList.toggle('itsYourTurn');
+    });
+  }
+
+  function players() {
+    return [player1, player2];
+  }
+
+  return {
+    players,
+    toggleTurn,
+  };
+})();
+
+const gameModule = (function () {
+  const fields = Array.from(document.querySelectorAll('.field'));
+
+  function returnSign() {
+    let sign = '';
+    const players = mainModule.players();
+    for (player of players) {
+      if (player.turn === true) {
+        sign = player.sign;
+      }
+    }
+    return sign;
+  }
+
+  function makeMove() {
+    this.innerHTML = returnSign();
+    mainModule.toggleTurn();
+  }
+
+  for (field of fields) {
+    field.addEventListener('click', makeMove);
+  }
 })();
